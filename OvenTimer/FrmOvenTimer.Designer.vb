6c6d4f577c820ff5f555f85b74e34778d69313ae
VB1975 <59953261+VB1975@users.noreply.github.com>
2024-08-11 21:01:32 +0100

    Add project files.

---
+++ OvenTimer/FrmOvenTimer.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmOvenTimer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmOvenTimer))
+        Me.PnlTimer = New System.Windows.Forms.Panel()
+        Me.TxtSeconds = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtMinutes = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtHours = New System.Windows.Forms.TextBox()
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.BtnReset = New System.Windows.Forms.Button()
+        Me.BtnStop = New System.Windows.Forms.Button()
+        Me.CboPresets = New System.Windows.Forms.ComboBox()
+        Me.TimTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PnlTimer.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PnlTimer
+        '
+        Me.PnlTimer.BackColor = System.Drawing.Color.White
+        Me.PnlTimer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PnlTimer.Controls.Add(Me.TxtSeconds)
+        Me.PnlTimer.Controls.Add(Me.Label2)
+        Me.PnlTimer.Controls.Add(Me.TxtMinutes)
+        Me.PnlTimer.Controls.Add(Me.Label1)
+        Me.PnlTimer.Controls.Add(Me.TxtHours)
+        Me.PnlTimer.Location = New System.Drawing.Point(17, 57)
+        Me.PnlTimer.Name = "PnlTimer"
+        Me.PnlTimer.Size = New System.Drawing.Size(325, 63)
+        Me.PnlTimer.TabIndex = 0
+        '
+        'TxtSeconds
+        '
+        Me.TxtSeconds.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TxtSeconds.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtSeconds.Location = New System.Drawing.Point(245, 3)
+        Me.TxtSeconds.MaxLength = 2
+        Me.TxtSeconds.Name = "TxtSeconds"
+        Me.TxtSeconds.Size = New System.Drawing.Size(72, 55)
+        Me.TxtSeconds.TabIndex = 2
+        Me.TxtSeconds.Text = "00"
+        Me.TxtSeconds.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(202, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 55)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = ":"
+        '
+        'TxtMinutes
+        '
+        Me.TxtMinutes.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TxtMinutes.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtMinutes.Location = New System.Drawing.Point(124, 3)
+        Me.TxtMinutes.MaxLength = 2
+        Me.TxtMinutes.Name = "TxtMinutes"
+        Me.TxtMinutes.Size = New System.Drawing.Size(72, 55)
+        Me.TxtMinutes.TabIndex = 1
+        Me.TxtMinutes.Text = "00"
+        Me.TxtMinutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(81, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 55)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = ":"
+        '
+        'TxtHours
+        '
+        Me.TxtHours.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TxtHours.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtHours.Location = New System.Drawing.Point(3, 3)
+        Me.TxtHours.MaxLength = 2
+        Me.TxtHours.Name = "TxtHours"
+        Me.TxtHours.Size = New System.Drawing.Size(72, 55)
+        Me.TxtHours.TabIndex = 0
+        Me.TxtHours.Text = "00"
+        Me.TxtHours.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'BtnStart
+        '
+        Me.BtnStart.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.BtnStart.Enabled = False
+        Me.BtnStart.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.BtnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnStart.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnStart.ForeColor = System.Drawing.Color.White
+        Me.BtnStart.Location = New System.Drawing.Point(18, 153)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(324, 82)
+        Me.BtnStart.TabIndex = 1
+        Me.BtnStart.Text = "Start"
+        Me.BtnStart.UseVisualStyleBackColor = False
+        '
+        'BtnReset
+        '
+        Me.BtnReset.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.BtnReset.Enabled = False
+        Me.BtnReset.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.BtnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnReset.ForeColor = System.Drawing.Color.White
+        Me.BtnReset.Location = New System.Drawing.Point(17, 329)
+        Me.BtnReset.Name = "BtnReset"
+        Me.BtnReset.Size = New System.Drawing.Size(324, 82)
+        Me.BtnReset.TabIndex = 3
+        Me.BtnReset.Text = "Reset"
+        Me.BtnReset.UseVisualStyleBackColor = False
+        '
+        'BtnStop
+        '
+        Me.BtnStop.BackColor = System.Drawing.Color.IndianRed
+        Me.BtnStop.Enabled = False
+        Me.BtnStop.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.BtnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnStop.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnStop.ForeColor = System.Drawing.Color.White
+        Me.BtnStop.Location = New System.Drawing.Point(17, 241)
+        Me.BtnStop.Name = "BtnStop"
+        Me.BtnStop.Size = New System.Drawing.Size(324, 82)
+        Me.BtnStop.TabIndex = 2
+        Me.BtnStop.Text = "Stop"
+        Me.BtnStop.UseVisualStyleBackColor = False
+        '
+        'CboPresets
+        '
+        Me.CboPresets.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CboPresets.FormattingEnabled = True
+        Me.CboPresets.Location = New System.Drawing.Point(17, 12)
+        Me.CboPresets.Name = "CboPresets"
+        Me.CboPresets.Size = New System.Drawing.Size(325, 39)
+        Me.CboPresets.TabIndex = 0
+        '
+        'TimTimer
+        '
+        Me.TimTimer.Interval = 1000
+        '
+        'Label3
+        '
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label3.Location = New System.Drawing.Point(21, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 21)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "h h"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label4.Location = New System.Drawing.Point(142, 120)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 21)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "m m"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label5.Location = New System.Drawing.Point(263, 120)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 21)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "s s"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'FrmOvenTimer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(363, 431)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.CboPresets)
+        Me.Controls.Add(Me.BtnStop)
+        Me.Controls.Add(Me.BtnReset)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Controls.Add(Me.PnlTimer)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(5, 6, 5, 6)
+        Me.MaximizeBox = False
+        Me.Name = "FrmOvenTimer"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Oven Timer"
+        Me.TopMost = True
+        Me.PnlTimer.ResumeLayout(False)
+        Me.PnlTimer.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PnlTimer As Panel
+    Friend WithEvents TxtHours As TextBox
+    Friend WithEvents TxtSeconds As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtMinutes As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnStart As Button
+    Friend WithEvents BtnReset As Button
+    Friend WithEvents BtnStop As Button
+    Friend WithEvents CboPresets As ComboBox
+    Friend WithEvents TimTimer As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ OvenTimer/FrmOvenTimer.vb
@@ -0,0 +1,235 @@
+﻿Imports System.Media
+
+Public Class FrmOvenTimer
+
+    Private player As New SoundPlayer(My.Resources.EmergencyAlarm)
+    Private TimerRunning As Boolean = False
+
+    Private Const DEFAULT_PRESET_TEXT As String = "Select A Preset..."
+
+    Private Sub FrmOvenTimer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Location = New Point(0, 10)
+        LoadPresets()
+        EnableButtons()
+        CboPresets.Text = DEFAULT_PRESET_TEXT
+
+    End Sub
+
+    Protected Overrides Sub OnFormClosed(e As FormClosedEventArgs)
+
+        player.Dispose()
+        MyBase.OnFormClosed(e)
+
+    End Sub
+
+    Private Sub LoadPresets()
+
+        CboPresets.Items.Clear()
+
+        CboPresets.Items.Add(DEFAULT_PRESET_TEXT)
+        CboPresets.Items.Add("15 Minutes")
+        CboPresets.Items.Add("30 Minutes")
+        CboPresets.Items.Add("45 Minutes")
+        CboPresets.Items.Add("1 Hour")
+        CboPresets.Items.Add("1.5 Hours")
+        CboPresets.Items.Add("2 Hours")
+        CboPresets.Items.Add("2.5 Hours")
+        CboPresets.Items.Add("3 Hours")
+
+    End Sub
+
+    Private Sub FormatPresets()
+
+        If CboPresets.Text = DEFAULT_PRESET_TEXT Then
+            CboPresets.ForeColor = Color.FromArgb(215, 215, 215)
+        Else
+            CboPresets.ForeColor = Color.FromArgb(0, 0, 0)
+        End If
+
+    End Sub
+
+    Private Sub RecolorForm()
+
+        If TimerRunning Then
+            BackColor = Color.Green
+        Else
+            BackColor = SystemColors.Control
+        End If
+
+    End Sub
+
+    Private Sub CboPresets_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CboPresets.SelectedIndexChanged
+
+        Dim H As String = "0", M As String = "0", S As String = "0"
+
+        BtnStart.Focus()
+        If CboPresets.Text = DEFAULT_PRESET_TEXT Then Exit Sub
+
+        Select Case CboPresets.SelectedItem
+            Case "15 Minutes" : M = "15"
+            Case "30 Minutes" : M = "30"
+            Case "45 Minutes" : M = "45"
+            Case "1 Hour" : H = "1"
+            Case "1.5 Hours" : H = "1" : M = "30"
+            Case "2 Hours" : H = "2"
+            Case "2.5 Hours" : H = "2" : M = "30"
+            Case "3 Hours" : H = "3"
+        End Select
+
+        TxtHours.Text = H : TxtMinutes.Text = M : TxtSeconds.Text = S
+        CboPresets.Text = DEFAULT_PRESET_TEXT
+
+        FormatPresets()
+        FormatTimer()
+        EnableButtons()
+        BtnStart.Focus()
+
+    End Sub
+
+    Private Sub EnableButtons()
+
+        If Val(TxtHours.Text) <> 0 Or Val(TxtMinutes.Text) <> 0 Or Val(TxtSeconds.Text) <> 0 Then
+            BtnStart.Enabled = True
+            BtnReset.Enabled = False
+            If TimerRunning Then
+                BtnStart.Enabled = False
+                BtnStop.Enabled = True
+            Else
+                BtnStart.Enabled = True
+                BtnStop.Enabled = False
+                BtnReset.Enabled = True
+            End If
+        Else
+            BtnStart.Enabled = False
+            If Not TimerRunning Then BtnReset.Enabled = False
+        End If
+
+        If TimerRunning Then
+            TxtHours.Enabled = False : TxtMinutes.Enabled = False : TxtSeconds.Enabled = False
+            PnlTimer.BackColor = SystemColors.Control
+            CboPresets.Enabled = False
+        Else
+            TxtHours.Enabled = True : TxtMinutes.Enabled = True : TxtSeconds.Enabled = True
+            PnlTimer.BackColor = Color.White
+            CboPresets.Enabled = True
+        End If
+
+        RecolorForm()
+
+    End Sub
+
+    Private Sub CboPresets_TextChanged(sender As Object, e As EventArgs) Handles CboPresets.TextChanged
+
+        FormatPresets()
+
+    End Sub
+
+    Private Sub CboPresets_DropDown(sender As Object, e As EventArgs) Handles CboPresets.DropDown
+
+        CboPresets.Text = ""
+        FormatPresets()
+
+    End Sub
+
+    Private Sub CboPresets_DropDownClosed(sender As Object, e As EventArgs) Handles CboPresets.DropDownClosed
+
+        If CboPresets.SelectedItem = "" Then
+            CboPresets.Text = DEFAULT_PRESET_TEXT
+            FormatPresets()
+        End If
+
+    End Sub
+
+    Private Sub FormatTimer()
+
+        If TxtSeconds.Text.Length = 1 Then TxtSeconds.Text = "0" & TxtSeconds.Text
+        If TxtMinutes.Text.Length = 1 Then TxtMinutes.Text = "0" & TxtMinutes.Text
+        If TxtHours.Text.Length = 1 Then TxtHours.Text = "0" & TxtHours.Text
+
+    End Sub
+
+    Private Sub TimTimer_Tick(sender As Object, e As EventArgs) Handles TimTimer.Tick
+
+        If Val(TxtHours.Text) <> 0 And Val(TxtMinutes.Text) = 0 And Val(TxtSeconds.Text) = 0 Then
+            TxtMinutes.Text = "60"
+            TxtHours.Text = Val(TxtHours.Text) - 1
+        End If
+
+        If Val(TxtSeconds.Text) = 0 Then
+            TxtMinutes.Text = Val(TxtMinutes.Text) - 1
+            TxtSeconds.Text = "60"
+        End If
+
+        TxtSeconds.Text = Val(TxtSeconds.Text) - 1
+        FormatTimer()
+
+        If Val(TxtHours.Text) = 0 And Val(TxtMinutes.Text) = 0 And Val(TxtSeconds.Text) = 0 Then
+            player.PlayLooping()
+            TimTimer.Enabled = False
+            TimerRunning = False
+            EnableButtons()
+            BackColor = Color.Red
+        End If
+
+    End Sub
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+
+        TimTimer.Enabled = True
+        TimerRunning = True
+        EnableButtons()
+        BtnStop.Focus()
+
+    End Sub
+
+    Private Sub BtnStop_Click(sender As Object, e As EventArgs) Handles BtnStop.Click
+
+        player.Stop()
+        TimTimer.Enabled = False
+        TimerRunning = False
+        EnableButtons()
+        BtnReset.Focus()
+        If Not TimerRunning Then BtnStop.Enabled = False
+
+    End Sub
+
+    Private Sub BtnReset_Click(sender As Object, e As EventArgs) Handles BtnReset.Click
+
+        TxtHours.Text = "00" : TxtMinutes.Text = "00" : TxtSeconds.Text = "00"
+        TimerRunning = False
+        EnableButtons()
+
+    End Sub
+
+    Private Sub TimerChanged(sender As Object, e As EventArgs) Handles TxtHours.TextChanged, TxtMinutes.TextChanged, TxtSeconds.TextChanged
+
+        If Not TimerRunning Then
+            If TxtHours.Text.Length = 2 And TxtMinutes.Text.Length = 2 And
+                TxtSeconds.Text.Length = 2 Then EnableButtons()
+        End If
+
+    End Sub
+
+    Private Sub CboPresets_TextUpdate(sender As Object, e As EventArgs) Handles CboPresets.TextUpdate
+
+        Dim X As Integer
+
+        For X = 0 To CboPresets.Items.Count - 1
+            If CboPresets.Text <> CboPresets.Items.Item(X) Then
+                CboPresets.Text = DEFAULT_PRESET_TEXT
+                Exit For
+            End If
+        Next
+
+    End Sub
+
+    Private Sub TxtSeconds_LostFocus(sender As Object, e As EventArgs) Handles TxtSeconds.LostFocus
+
+        If TxtHours.Text = "" Or Not IsNumeric(TxtHours.Text) Then TxtHours.Text = "00"
+        If TxtMinutes.Text = "" Or Not IsNumeric(TxtMinutes.Text) Then TxtMinutes.Text = "00"
+        If TxtSeconds.Text = "" Or Not IsNumeric(TxtSeconds.Text) Then TxtSeconds.Text = "00"
+        EnableButtons()
+
+    End Sub
+End Class
